c6af64b7724385c9f541646348987c9c74fe9b1a
manorajesh <mano@raje.sh>
2022-11-21 09:55:32 -0800

    Fixing bugs in crates.io repo

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{io::{stdin, Read}, fs::File, collections::HashMap};
 
 #[derive(Parser)]
 #[command(
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Mano Rajesh",
     about = "Program to count character frequency from FILE or STDIN"
 )]
